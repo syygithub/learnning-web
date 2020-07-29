1745277ef5e71798a9ab9ad32b120404ce01812a
songyy <songyy@zctt.com>
2020-07-29 11:54:08 +0800

    first init

---
+++ lesson1/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import DialogPage from "./pages/DialogPage";
+
+export default function App(props) {
+  return (
+    <div>
+      {/* <ContextPage /> */}
+      {/* <AntdFormPage /> */}
+      {/* <MyRCFieldForm /> */}
+      <DialogPage />
+    </div>
+  );
+}
